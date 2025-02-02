19de7ef328515290513e3fb4ce6e6720c228687d
mchristy-mufin <m.christy@mufin.co.kr>
2025-02-02 12:19:12 +0530

    removed commented code

--- src/sections/PopOverSection/PopOverSection.tsx
+++ src/sections/PopOverSection/PopOverSection.tsx
@@ -26,22 +26,6 @@ const PopOverSection = (props: { sections: string[]; sectionRefs: MutableRefObje
           }}
         >
           <div className={`flex w-[80%] ${index % 2 === 0 ? "justify-start" : "justify-end"}`}>
-            {/* <motion.div
-              className="w-[40%] h-[250px] rounded-lg shadow-lg bg-[#c66a10] p-4"
-              initial={{ x: index % 2 === 0 ? "-100vw" : "100vw", y: "-100vh", opacity: 0 }}
-              animate={activeIndex === index ? { x: 0, y: 0, opacity: 1 } : { opacity: 0 }}
-              transition={{ duration: 2, ease: "easeInOut" }}
-            >
-              <div className={`flex flex-col ${index % 2 === 0 ? "items-start" : "items-end text-end"}`}>
-                <h2 className="font-serif text-4xl font-normal leading-tight text-[#efede7]">A Journey Within</h2>
-                <p className="font-sans text-base font-normal leading-6 tracking-[0.5px] text-[#efede7]">
-                  Pujya Gurudevshri has revitalised the path to self-realisation. Each spiritual activity taps into a
-                  deeper dimension, allowing you to bloom into a fuller existence.
-                </p>
-                <button className="rounded-lg bg-white text-left font-sans text-base p-2 mt-4">Discover more</button>
-              </div>
-            </motion.div> */}
-
             <motion.div
               className="w-[40%] h-auto rounded-lg shadow-lg bg-[#c66a10] p-4"
               initial={{ x: index % 2 === 0 ? "-100vw" : "100vw", y: "-100vh", opacity: 0 }}
